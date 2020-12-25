9ac6575c6fe1b799a30078739c3aaa17ca9fd232
D_S <69339800+Dark-Star321@users.noreply.github.com>
2020-12-25 17:01:10 +0900

    変数の名前を変更

--- integerToSuuji.js
+++ integerToSuuji.js
@@ -1,11 +1,11 @@
 ﻿function integerToSuuji(inte) {
 	var strNum = String(inte);
-	var namae = ["", "万", "億", "兆", "京", "垓"];
-	var keta = Math.floor((strNum.length-1) / 4);
+	var keta = ["", "万", "億", "兆", "京", "垓"];
+	var digit = Math.floor((strNum.length-1) / 4);
 	var num = "";
 	for (var i=0; i<=(strNum.length-1) % 4; i++) {
 		num += strNum.substring(i, i+1);
 	}
 	console.log (strNum.length);
-	return num + namae[keta];
+	return num + keta[digit];
 }
